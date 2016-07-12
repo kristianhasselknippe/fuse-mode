54cb7c60071f57f384551975bda73fe2fb6045b6
Haskellknippe <kristian_hasselknippe@outlook.com>
2016-07-12 22:47:10 +0200

    foo

--- fuse-ux-parser.el
+++ fuse-ux-parser.el
@@ -1,35 +1,46 @@
 (defvar ux-buffer "")
 (defvar ux-pos 0)
 
-(defun fuse/consume (c)
+(defun fuse-consume (c)
   (setf ux-pos (+ ux-pos c)))
 
-(defun fuse/consume ()
-  (fuse/consume 1))
+(defun fuse-consume ()
+  (fuse-consume 1))
 
-(defun fuse/parse-char (c)
+(defun fuse-parse-char (c)
   (if (eq c (aref ux-buffer ux-pos))
-	  (fuse/consume)
+	  (fuse-consume)
 	'nil))
 
-(defun fuse/parse-string (s)
-  (contains ux-buffer))
+(defun fuse-parse-string (s)
+  (while (fuse-parse-char ()
 
-(defun fuse/parse-element (ast)
+(defun fuse-parse-element (ast)
+  (fuse-parse-string "<"))
+
+(defun fuse-parse-root (ast)
   )
 
-(defun fuse/parse-root (ast)
-  )
-
-(defun fuse/parse-ux (ux)
+(defun fuse-parse-ux (ux)
   (setf ux-buffer ux)
-  (fuse/parse-root))
+  (fuse-parse-root))
 
-(defun fuse/test-ux (ux procedure &rest args)
+(defun fuse-prepare-test-ux (ux)
   (setf ux-buffer ux)
-  (setf ux-pos 0)
-  (apply procedure args))
+  (setf ux-pos 0))
 
+(defun fuse-print-ast (ast)
+  (princ ast))
 
-(defun fuse/test-ux-1 ()
-  (fuse/test-ux "<App><Panel></Panel></App>" 'fuse/parse-root))
+(defun fuse-test-string ()
+  (fuse-prepare-test-ux "<App>")
+  (fuse-parse-string "<App>"))
+
+(defun fuse-test-element ()
+  (fuse-prepare-test-ux "<App>")
+  (let ((ret (fuse-parse-element '())))
+	(fuse-print-ast ret)))
+
+(defun fuse-test-ux-1 ()
+  (fuse-prepare-test-ux "<App><Panel></Panel></App>")
+  (fuse-parse-root '()))
